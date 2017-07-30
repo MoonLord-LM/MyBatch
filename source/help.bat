c2393eacb2d68390bc5cf326578051bde356b3be
月之领主LM <178910432@qq.com>
2017-07-30 14:19:38 +0800

    Update

--- source/help.bat
+++ source/help.bat
@@ -5,19 +5,27 @@ setlocal enabledelayedexpansion
 ::
 del /q "help\*"
 ::
+mkdir "tmp\"
+::
+echo :: ÁÙÊ±½Å±¾>tmp.bat
+::
 for %%i in (
 
-del dir for set
-call copy goto tree
-shift
-setlocal
+at cd if rd
+cls cmd del dir for rem ren set ver vol
+call chcp comp copy date echo exit find goto mode mode path ping popd sort time tree type
+assoc color erase ftype label mkdir pause print pushd rmdir shift start subst title xcopy
+attrib chkdsk compact convert format prompt rename verify
+chkntfs netstat recover replace
+endlocal diskcomp diskcopy ipconfig setlocal
 
 ) do (
 
-echo %%i /? ^>"help\%%i.txt" && ^
-echo %%i /? ^>"help\%%i.txt" >tmp.bat && ^
-call tmp.bat
-del /q "tmp.*"
+echo echo %%i /? ^^^>"help\%%i.txt" 2^^^>^^^&1 1>>"tmp\help.tmp.bat" && ^
+echo %%i /? ^>"help\%%i.txt" 2^>^&1 1>>"tmp\help.tmp.bat"
 
 )
+call "tmp\help.tmp.bat"
+REM del /q "tmp\help.tmp.bat"
+pause
 ::
