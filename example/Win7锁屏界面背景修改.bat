77123797d2ada3787ae695b18d096532bb9a2818
月之领主LM <178910432@qq.com>
2017-09-10 23:57:16 +0800

    Update

--- example/Win7锁屏界面背景修改.bat
+++ example/Win7锁屏界面背景修改.bat
@@ -6,12 +6,20 @@ pause
 exit
 )
 call :AdministratorPrivileges "%~0" "%~1" "%~2" "%~3" "%~4" "%~5" "%~6" "%~7" "%~8" "%~9"
-set tmp=C:\Windows\Temp\LogonUI_Background.reg
+set tmp="C:\Windows\Temp\LogonUI_Background_%random%.reg"
 echo Windows Registry Editor Version 5.00>%tmp%
 echo [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background]>>%tmp%
 echo "OEMBackground"=dword:00000001>>%tmp%
 reg import %tmp%
-set bg="%~1 %~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9"
+set bg="%~1"
+if "%~2" neq "" ( set bg="%~1 %~2" )
+if "%~3" neq "" ( set bg="%~1 %~2 %~3" )
+if "%~4" neq "" ( set bg="%~1 %~2 %~3 %~4" )
+if "%~5" neq "" ( set bg="%~1 %~2 %~3 %~4 %~5" )
+if "%~6" neq "" ( set bg="%~1 %~2 %~3 %~4 %~5 %~6" )
+if "%~7" neq "" ( set bg="%~1 %~2 %~3 %~4 %~5 %~6 %~7" )
+if "%~8" neq "" ( set bg="%~1 %~2 %~3 %~4 %~5 %~6 %~7 %~8" )
+if "%~9" neq "" ( set bg="%~1 %~2 %~3 %~4 %~5 %~6 %~7 %~8 %~9" )
 copy %bg% "C:\Windows\System32\oobe\info\backgrounds\backgroundDefault.jpg" /y
 if %errorlevel% equ 0 ( echo Win7锁屏界面背景修改成功，图片文件为：%bg% ) else ( echo Win7锁屏界面背景修改失败，图片文件为：%bg% )
 pause
@@ -20,7 +28,7 @@ exit
 :AdministratorPrivileges - "获取管理员权限"
     net file 1>nul 2>nul
     if %errorlevel% equ 0 ( echo 已获取管理员权限 && goto :eof ) else ( echo 需要获取管理员权限，请确认…… )
-    set vbs=C:\Windows\Temp\Get_Privileges.vbs
+    set vbs="C:\Windows\Temp\Get_Privileges_%random%.vbs"
     echo Set UAC = CreateObject^("Shell.Application"^)>%vbs%
     echo Args = "">>%vbs%
     echo For Each Argument In WScript.Arguments>>%vbs%
