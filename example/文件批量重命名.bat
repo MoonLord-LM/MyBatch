603f2d37438eb0d29d00b57de1221643919eca8c
月之领主LM <178910432@qq.com>
2018-12-09 03:03:55 +0800

    Update

---
+++ example/文件批量重命名.bat
@@ -0,0 +1,61 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM //处理的文件后缀
+set "extension=*.mp4,*.wmv,*.mkv,*.avi"
+echo.>C:\Windows\Temp\file_batch_rename.bat
+
+for /r "%cd%" %%i in (%extension%) do (
+
+    REM //遍历文件名
+    set file_path=%%~i
+    call :filepath_to_filename
+    REM echo file_name : !file_name!
+
+    REM //替换字符串
+    set "new_name=!file_name!"
+    call :new_name_replace "[Thz.la]"
+    call :new_name_replace "[ThZu.Cc]"
+    call :new_name_replace "hjd2048.com-"
+    call :new_name_replace "-h264"
+    call :new_name_replace "-1080p"
+    REM echo new_name : !new_name!
+
+    REM //重命名脚本
+    if not "!file_name!"=="!new_name!" (
+        echo rename "!file_path!" "!new_name!" >>C:\Windows\Temp\file_batch_rename.bat
+        echo !file_name! ---^> !new_name!
+    )
+)
+
+echo.
+echo type "C:\Windows\Temp\file_batch_rename.bat"
+type "C:\Windows\Temp\file_batch_rename.bat"
+echo.
+echo 请确认重命名脚本，请按任意键继续执行. . .
+pause
+call "C:\Windows\Temp\file_batch_rename.bat"
+exit
+
+:filepath_to_filename - "将完整的文件路径(file_path)转换为文件名(file_name)"
+    set /a tmp_offset=1
+    :loop
+    call set "tmp_mark=%%file_path:~-!tmp_offset!,1%%%"
+    REM echo tmp_mark : !tmp_mark!
+    if not "!tmp_mark!"=="" (
+        if not "!tmp_mark!" == "\" (
+            set /a tmp_offset+=1
+            goto loop
+        )
+    )
+    set /a tmp_offset-=1
+    call set "file_name=%%file_path:~-!tmp_offset!%%%"
+goto :eof
+
+:new_name_replace - "将新的文件名(new_name)中的特定字符串(参数)删除"
+    if not "%~1"=="" (
+        set "old_tag=%~1"
+        set "new_tag=%~2"
+        call set "new_name=%%new_name:!old_tag!=!new_tag!%%"
+    )
+goto :eof
