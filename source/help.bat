17eef43891a78f1e87b448ac23b077e1a2990867
月之领主LM <178910432@qq.com>
2018-12-16 03:37:21 +0800

    Update

--- source/help.bat
+++ source/help.bat
@@ -11,13 +11,14 @@ echo :: 临时脚本>"tmp\help.tmp.bat"
 ::
 for %%i in (
 
-at cd if rd
-arp cls cmd del dir for net rem ren set ver vol
-call chcp comp copy date echo exit find goto mode mode path ping popd sort time tree type wmic
-assoc color erase ftype label mkdir pause print pushd rmdir shift start subst title xcopy
-attrib chkdsk compact convert format prompt rename verify
-chkntfs netstat recover replace
-certutil endlocal diskcomp diskcopy ipconfig setlocal tracert
+at cd fc if rd
+arp cls cmd del dir for ftp net rem ren set ver vol
+call chcp clip comp copy date echo exit find goto mode mode path ping popd sort time tree type wmic
+assoc cacls color erase fltmc ftype label mkdir pause print pushd rmdir shift start subst title xcopy
+attrib change chkdsk choice cipher defrag doskey expand format fsutil getmac icacls makecab prompt rename verify
+bcdboot bcdedit certreq chkntfs compact convert findstr netstat recover replace
+certutil endlocal diskcomp diskcopy forfiles gpresult gpupdate hostname ipconfig setlocal tracert
+eventcreate
 
 ) do (
 
@@ -28,4 +29,8 @@ echo %%i /? ^>"help\%%i.txt" 2^>^&1 1>>"tmp\help.tmp.bat"
 call "tmp\help.tmp.bat"
 REM del /q "tmp\help.tmp.bat"
 pause
-::
+
+:: TODO
+:: 例如 ftp 的命令帮助无法输出到文件
+:: 例如 change 需要多一个参数才能输出详细帮助
+:: 例如 dism 需要权限提升才能运行
