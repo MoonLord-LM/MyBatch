cdb4dbfef61c62ecf824f8524cc7bac05de7493d
月之领主LM <178910432@qq.com>
2018-12-20 21:29:03 +0800

    Update

--- example/管理员权限CMD.bat
+++ example/管理员权限CMD.bat
@@ -1,6 +1,7 @@
 @echo off
 call :AdministratorPrivileges "%~0" "%~1" "%~2" "%~3" "%~4" "%~5" "%~6" "%~7" "%~8" "%~9"
 pushd %~dp0
+
 cmd
 
 :AdministratorPrivileges - "获取管理员权限"
--- source/help.bat
+++ source/help.bat
@@ -1,23 +1,28 @@
 @echo off
 setlocal enabledelayedexpansion
+
 ::
 ::  更新【help】文件夹中的所有的命令帮助信息
 ::
+
+call :AdministratorPrivileges "%~0" "%~1" "%~2" "%~3" "%~4" "%~5" "%~6" "%~7" "%~8" "%~9"
+pushd %~dp0
+
 del /q "help\*"
-::
-mkdir "tmp\"
-::
+if not exist "tmp\" ( mkdir "tmp\" )
 echo :: 临时脚本>"tmp\help.tmp.bat"
-::
+
 for %%i in (
 
-at cd fc if rd
-arp cls cmd del dir for ftp net rem ren set ver vol
-call chcp clip comp copy date echo exit find goto mode mode path ping popd sort time tree type wmic
-assoc cacls color erase fltmc ftype label mkdir pause print pushd rmdir shift start subst title xcopy
-attrib change chkdsk choice cipher defrag doskey expand format fsutil getmac icacls makecab prompt rename verify
-bcdboot bcdedit certreq chkntfs compact convert findstr netstat recover replace
-certutil endlocal diskcomp diskcopy forfiles gpresult gpupdate hostname ipconfig setlocal tracert
+at cd fc if md rd
+arp cls cmd del dir for msg net rem ren set sfc ver vol
+call chcp clip comp copy date dism echo exit find goto mode more move path ping popd setx sort time tree type wmic
+assoc cacls color erase fltmc ftype label mkdir pause print pushd quser rmdir shift start subst title w32tm where xcopy
+attrib change chkdsk choice cipher defrag doskey expand format fsutil getmac icacls lodctr logman logoff mklink netsh prompt rename setspn tskill tzutil verify whoami
+bcdboot bcdedit certreq chkntfs compact convert findstr makecab nbtstat netstat nltest qappsrv qwinsta recover replace takeown timeout tracert waitfor wbadmin wecutil
+certutil endlocal diskcomp diskcopy forfiles gpresult gpupdate hostname ipconfig mountvol nslookup pathping powercfg qprocess schtasks setlocal shutdown tasklist taskkill tsdiscon typeperf vssadmin wevtutil
+openfiles
+systeminfo
 eventcreate
 
 ) do (
@@ -29,8 +34,23 @@ echo %%i /? ^>"help\%%i.txt" 2^>^&1 1>>"tmp\help.tmp.bat"
 call "tmp\help.tmp.bat"
 REM del /q "tmp\help.tmp.bat"
 pause
+exit
 
 :: TODO
-:: 例如 ftp 的命令帮助无法输出到文件
 :: 例如 change 需要多一个参数才能输出详细帮助
-:: 例如 dism 需要权限提升才能运行
+:: 例如 sc 需要输入一个 Y 进行确认
+::  winrm
+
+:AdministratorPrivileges - "获取管理员权限"
+    net file 1>nul 2>nul
+    if %errorlevel% equ 0 ( echo 已获取管理员权限 && goto :eof ) else ( echo 需要获取管理员权限，请确认…… )
+    set vbs="C:\Windows\Temp\Get_Privileges_%random%.vbs"
+    echo Set UAC = CreateObject^("Shell.Application"^)>%vbs%
+    echo Args = "">>%vbs%
+    echo For Each Argument In WScript.Arguments>>%vbs%
+    echo Args = Args ^& Argument ^& " ">>%vbs%
+    echo Next>>%vbs%
+    echo UAC.ShellExecute "%~1", Args, "", "runas", 1 >>%vbs%
+    "%SystemRoot%\System32\WScript.exe" %vbs% "%~2" "%~3" "%~4" "%~5" "%~6" "%~7" "%~8" "%~9"
+    exit
+goto :eof
