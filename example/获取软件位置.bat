176474975df2ddbc0082e97afa9bb197703ea086
月之领主LM <178910432@qq.com>
2019-01-26 15:57:26 +0800

    Update

--- example/获取软件位置.bat
+++ example/获取软件位置.bat
@@ -1,5 +1,6 @@
 @echo off
 
+:: 查找注册表，找到 Visual Studio Code 的 Code.exe 的位置
 for /f usebackq^ tokens^=1^,2^,*^ delims^=^" %%i ^
 in (`reg query "HKCU\Software\Classes\VSCodeSourceFile\shell\open\command" /ve`) ^
 do set "vscode_exe=%%j"
