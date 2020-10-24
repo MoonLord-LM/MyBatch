3ad4a61d151d2c48016d761ec8481197a3503c7b
MoonLord-LM <178910432@qq.com>
2020-10-25 00:24:38 +0800

    Update

--- example/屏蔽游侠链接.bat
+++ example/屏蔽游侠链接.bat
@@ -4,8 +4,8 @@ echo 通过建立同名的隐藏文件夹，来阻止破解版游戏自动创建游侠的链接文件
 set adv_name=游侠热门单机游戏.url
 
 del /F /S /Q %adv_name%
-rd /S /Q %adv_name%
-md %adv_name%
+rmdir /S /Q %adv_name%
+mkdir %adv_name%
 attrib +h +s %adv_name%
 
 exit
