962927a8fc6fdd16e96b96cc51f910a06fc8f243
月之领主LM <178910432@qq.com>
2021-12-12 16:31:34 +0800

    Update

---
+++ example/FFmpeg 转换 flv 到 mp4.bat
@@ -0,0 +1,17 @@
+@echo off
+echo 使用 FFmpeg，转换视频格式，从 flv 到 mp4
+
+if "%~1"=="" (
+    echo 请拖拽文件，到本文件图标上运行
+    pause
+    exit
+)
+
+set "ffmpeg=S:\Common\DouyuPCClient\Client\8.5.2.2\ffmpeg.exe"
+echo "%ffmpeg%" -i "%~1" -c copy "%~1.mp4"
+
+"%ffmpeg%" -i "%~1" -c copy "%~1.mp4"
+
+echo 转换完成，请查看生成的 mp4 文件
+pause
+exit
