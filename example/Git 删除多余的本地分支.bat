26c658ee56eb688c845dbcd62efaf7ddbc6802d6
月之领主LM <me@moonlord.cn>
2022-01-10 14:48:34 +0800

    Update

--- example/Git 删除多余的本地分支.bat
+++ example/Git 删除多余的本地分支.bat
@@ -1,14 +1,25 @@
 @echo off
 
-:: MoonLord 2021.10.29  
-:: 根据文件夹后缀（-master、-develop、-ICSL），判断当前是哪个分支  
+:: MoonLord 2022.01.10   
+:: 根据文件夹后缀（-master、-develop、-release、-dev），判断当前是哪个分支  
+:: 无文件夹后缀，则默认为 master 分支
 :: 删除多余的本地分支  
 :: 仅保留一个主要的分支  
+
+:: dir /AD /B >dir.txt
 
 echo ---- > "%tmp%\branch.txt"
 for /f usebackq %%i in ( `dir /AD /B` ) do (
     echo "processing %%i ..."
     cd "%%i"
+    echo "%%i" | findstr "\-\>" >nul || (
+        for /f usebackq %%j in ( `git branch` ) do (
+            echo "%%j" | findstr "* master\>" >nul || (
+                echo "branch to be deleted: %%j"
+                git branch -D "%%j"
+            )
+        )
+    )
     echo "%%i" | findstr "\-master\>" >nul && (
         for /f usebackq %%j in ( `git branch` ) do (
             echo "%%j" | findstr "* master\>" >nul || (
@@ -25,9 +36,17 @@ for /f usebackq %%i in ( `dir /AD /B` ) do (
             )
         )
     )
-    echo "%%i" | findstr "\-ICSL\>" >nul && (
+    echo "%%i" | findstr "\-release\>" >nul && (
         for /f usebackq %%j in ( `git branch` ) do (
-            echo "%%j" | findstr "* ICSL\>" >nul || (
+            echo "%%j" | findstr "* release\>" >nul || (
+                echo "branch to be deleted: %%j"
+                git branch -D "%%j"
+            )
+        )
+    )
+    echo "%%i" | findstr "\-dev\>" >nul && (
+        for /f usebackq %%j in ( `git branch` ) do (
+            echo "%%j" | findstr "* dev\>" >nul || (
                 echo "branch to be deleted: %%j"
                 git branch -D "%%j"
             )
