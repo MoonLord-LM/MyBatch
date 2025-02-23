bcdf9e5d38b92cd241f7c6087ec6ff93ad4453b0
MoonLord-LM <me@moonlord.cn>
2025-02-24 01:25:22 +0800

    Update

--- example/Git 本地二进制仓库优化.bat
+++ example/Git 本地二进制仓库优化.bat
@@ -12,6 +12,7 @@ git config --local core.bigFileThreshold 100m
 
 :: 压缩算法级别设置为无压缩
 git config --local core.compression 0
+git config --local core.looseCompression 0
 git config --local pack.compression 0
 
 :: 文件打包大小限制（如果单个文件大小超出限制，仍然会生成很大的 pack 文件）
