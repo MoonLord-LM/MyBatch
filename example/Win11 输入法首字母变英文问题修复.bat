89076fbd29e16f9480dc24475bd67c01d17cb159
MoonLord-LM <me@moonlord.cn>
2025-05-17 21:30:45 +0800

    Update

--- example/Win11 输入法首字母变英文问题修复.bat
+++ example/Win11 输入法首字母变英文问题修复.bat
@@ -1,20 +1,25 @@
 @echo off
-:: 设置注册表路径
-set "RegPath=HKEY_CURRENT_USER\Software\Microsoft\InputMethod\Settings\Common"
 
-:: 检查当前注册表项是否存在
+start "" "C:\Program Files\Common Files\microsoft shared\ink\TabTip.exe"
+echo 已启动触摸键盘，临时解决问题
+
+
+
+:: 检查注册表配置 TouchKeyboardHasEverShown
+set "RegPath=HKEY_CURRENT_USER\Software\Microsoft\InputMethod\Settings\Common"
 reg query "%RegPath%" | findstr /i "TouchKeyboardHasEverShown"
 reg query "%RegPath%" | findstr /i "TouchKeyboardHasEverShown" >nul 2>&1
 
 if %errorlevel% equ 0 (
-    echo 正在修改 TouchKeyboardHasEverShown 的值为 0...
     reg add "%RegPath%" /v TouchKeyboardHasEverShown /t REG_DWORD /d 0 /f >nul
-    echo 修改完成。
+    reg query "%RegPath%" | findstr /i "TouchKeyboardHasEverShown"
+    echo 已重置 TouchKeyboardHasEverShown 注册表项，后续请不要打开触模键盘
 ) else (
-    echo 未找到 TouchKeyboardHasEverShown 注册表项，可能需要手动检查路径。
+    echo 未找到 TouchKeyboardHasEverShown 注册表项，无需重置
 )
 
+
+
 echo.
-echo 请重启计算机或注销当前用户以使更改生效。
 pause
 exit
