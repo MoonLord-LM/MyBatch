c6d6c1d3588ac51809ee998bc3d96294a2cad968
MoonLord-LM <me@moonlord.cn>
2025-12-06 01:39:41 +0800

    Update

--- example/将子文件夹的文件移动出来.bat
+++ example/将子文件夹的文件移动出来.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 set "destination=%cd%"
 
-for /r %%i in (*.mp4 *.wmv *.mkv *.avi *.rar *.ts) do (
+for /r %%i in (*.mp4 *.mkv *.wmv *.mpg *.avi *.mpeg *.rar) do (
     move "%%i" "!destination!"
 )
 
