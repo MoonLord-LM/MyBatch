cb49e6e880fbba9ab8384b8cca363dbd51630ca9
MoonLord-LM <me@moonlord.cn>
2025-12-16 21:51:59 +0800

    Update

--- example/视频封面移除.bat
+++ example/视频封面移除.bat
@@ -4,27 +4,45 @@ setlocal enabledelayedexpansion
 :: 依赖的软件如下
 :: https://github.com/FFmpeg/FFmpeg
 
+echo.
+echo 本脚本用于移除视频封面
+echo 可输入文件路径，将想要处理的视频文件拖拽到窗口中
+echo 或者输入 mp4 这三个字母，以处理当前目录下的所有 mp4 文件
+echo.
+
 
 
 :loop
-echo 请将要处理的视频文件拖拽到窗口中
-set /p "url="
+    echo 请输入视频文件路径或 mp4：
 
-if "!url!"=="" (
-    echo 输入不能为空，请重新输入
-    goto loop
-)
+    set /p "url="
+    if "!url!"=="" (
+        echo 输入不能为空，请重新输入
+        goto loop
+    )
 
-:: 启动一个新的命令行窗口
-echo 在新窗口中处理: !url!
-set command=ffmpeg.exe ^
- -i !url! ^
- -map 0:v:0 ^
- -map 0:a:0 ^
- -c copy ^
- -map -0:d:2 ^
- !url!.new.mp4
-start "" cmd /c "%command%"
-echo.
+    if /i "!url!"=="mp4" (
+        for %%f in (*.mp4) do (
+            echo 处理: %%f
+            ffmpeg.exe -i "%%f" -c copy -map 0 -map -0:t "temp_%%f"
+            if !errorlevel! equ 0 (
+                del /f /q "%%f" >nul
+                ren "temp_%%f" "%%f" >nul
+                echo 封面移除成功
+            ) else (
+                echo 封面移除失败
+                if exist "temp_%%f" del /f /q "temp_%%f" > nul
+            )
+            echo.
+        )
+    ) else if exist "!url!" (
+        echo 处理: "!url!"
+        for %%f in ("!url!") do (
+            ffmpeg.exe -i "%%f" -c copy -map 0 -map -0:t "%%~dpnf.nocover.mp4"
+        )
+    ) else (
+        echo 错误的输入
+    )
 
+    echo.
 goto loop
