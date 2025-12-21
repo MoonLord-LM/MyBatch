073967de4b311abfd9b5ef53389d9bcb1fdde1d7
MoonLord-LM <me@moonlord.cn>
2025-12-21 22:26:24 +0800

    Update

--- example/将子文件夹的文件移动出来.bat
+++ example/将子文件夹的文件移动出来.bat
@@ -1,12 +1,17 @@
 @echo off
+chcp 65001
 setlocal enabledelayedexpansion
 
-set "destination=%cd%"
+:: 子目录中包含特殊符号，如 ! 时，会报错找不到目录，因此需要使用 endlocal
+
+
 
 for /r %%i in (*.mp4 *.mkv *.wmv *.mpg *.avi *.mpeg *.rar) do (
-    move "%%i" "!destination!"
+    endlocal
+    echo 处理文件: "%%i"
+    move "%%i" "%cd%"
+    setlocal enabledelayedexpansion
 )
 
-endlocal
 pause
 exit
