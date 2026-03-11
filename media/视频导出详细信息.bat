5ebc172cff4dc85e5d85d677f56945454ad4c207
MoonLord-LM <me@moonlord.cn>
2026-03-11 10:48:57 +0800

    Update

--- media/视频导出详细信息.bat
+++ media/视频导出详细信息.bat
@@ -6,8 +6,9 @@ powershell -NoProfile -Command "Write-Host '[ %~nx0 ]' -ForegroundColor Cyan" &&
 
 
 :: 导出视频详细信息为 json
-:: 双击运行时，自动扫描并处理当前目录下所有格式的视频文件
+:: 双击运行时，自动递归扫描和处理当前目录下所有的视频文件
 :: 拖拽单个视频文件到此脚本上时，则只处理该文件
+:: 支持的格式为 mp4 mkv ts avi wmv flv rmvb rm vob mpg mpeg 3gp m4v f4v mov webm
 
 
 
@@ -29,8 +30,7 @@ if errorlevel 1 (
 
 
 if "%~1" == "" (
-    echo.
-    echo 未检测到输入文件，将自动扫描并处理当前目录下的所有视频文件。
+    echo 开始扫描
     echo.
     for /r %%f in (*.mp4 *.mkv *.ts *.avi *.wmv *.flv *.rmvb *.rm *.vob *.mpg *.mpeg *.3gp *.m4v *.f4v *.mov *.webm) do (
         call :process_file "%%f"
@@ -48,23 +48,24 @@ pause
 exit /b
 
 
+
 :process_file
     set "file_path=%~1"
-    if not exist "%file_path%" goto :eof
+    if not exist "%file_path%" (
+        echo 文件不存在: %file_path%
+        goto :eof
+    )
 
     setlocal disabledelayedexpansion
     set "file_name=%~nx1"
     set "file_dir_path=%~dp1"
-    set "file_base_name=%~n1"
+    set "output_file=%~nx1.json"
     setlocal enabledelayedexpansion
 
-    if not "!file_dir_path!"=="" cd /d "!file_dir_path!"
-
     echo 正在处理: !file_name!
-    
-    set "output_file=!file_base_name!.json"
+    if not "!file_dir_path!"=="" ( cd /d "!file_dir_path!" )
     ffprobe -v error -show_streams -show_format -print_format json "!file_name!" > "!output_file!"
-    echo 详细信息已保存到: !output_file!
+    echo 已保存文件: !output_file!
 
     endlocal
     endlocal
