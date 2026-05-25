f59e453d9bc08b598f79942513cec509658e51a1
MoonLord-LM <me@moonlord.cn>
2026-05-25 12:08:54 +0800

    Update

---
+++ example/Powershell 允许当前用户执行 ps1 脚本.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+
+:: Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+powershell -NoProfile -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
+if %errorlevel% equ 0 (
+    echo 策略设置成功，已允许当前用户执行 ps1 脚本
+) else (
+    echo 策略设置设置失败，错误代码：%errorlevel%
+)
+
+pause
