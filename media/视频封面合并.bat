02a59fcde8e8a9862292ec5cd214dc77490f088e
MoonLord-LM <me@moonlord.cn>
2026-08-08 20:11:35 +0800

    Update

--- media/视频封面合并.bat
+++ media/视频封面合并.bat
@@ -5,7 +5,7 @@ powershell -NoProfile -Command "Write-Host '[ %~nx0 ]' -ForegroundColor Cyan" &&
 
 
 
-powershell -NoProfile -Command "Write-Host '合并视频封面' -ForegroundColor Green"
+powershell -NoProfile -Command "Write-Host '将与视频文件名同名的图片文件，设置为视频的封面' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '双击运行时，自动递归扫描和处理当前目录下所有的视频文件' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '拖拽单个视频文件到此脚本上时，则只处理该文件' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '支持的格式为 mp4 mkv ts avi wmv flv rmvb rm vob mpg mpeg 3gp m4v f4v mov webm' -ForegroundColor Green"
