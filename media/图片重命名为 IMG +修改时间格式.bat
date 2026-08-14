b696d61e583e8dc1c054b04df592762ceee8af0b
MoonLord-LM <me@moonlord.cn>
2026-08-14 11:43:13 +0800

    Update

--- media/图片重命名为 IMG +修改时间格式.bat
+++ media/图片重命名为 IMG +修改时间格式.bat
@@ -60,7 +60,7 @@ if "%~1" == "" (
             echo set /a "skipped+=1">> "!temp_set!"
             echo 时间获取失败，跳过此文件
         ) else (
-            for /f "delims=" %%l in ('powershell -NoProfile -Command "'!file_ext!'.ToLower()"') do set "lower_file_ext=%%l"
+            for /f "delims=" %%l in ('powershell -NoProfile -Command "$env:file_ext.ToLower()"') do set "lower_file_ext=%%l"
             set "new_name=IMG_!formatted_time!!lower_file_ext!"
             echo 目标文件名: "!new_name!"
             if /i "!img_file!"=="!file_dir!!new_name!" (
