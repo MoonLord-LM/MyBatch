b85b64ff6f880df16989920e137c18e86d366f99
MoonLord-LM <me@moonlord.cn>
2026-08-17 04:46:18 +0800

    Update

---
+++ example/视频的音画内容替换.bat
@@ -0,0 +1,41 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+
+
+
+:: ffmpeg -i 1.mp4 -i 2.mp4 -map 1:v:0 -map 1:a -map 0:2 -map_metadata 0 -c copy -disposition:v:1 attached_pic output.mp4
+
+echo.
+echo 请输入原始视频文件路径（包含封面、描述等）
+set /p "video1="
+
+if not exist "!video1!" (
+    echo 文件 "!video1!" 不存在，请检查路径后重试。
+    pause
+    exit /b
+)
+
+echo.
+echo 请输入用于替换视频流的文件路径（包含更高清的视频和更高质量的音频）
+set /p "video2="
+
+if not exist "!video2!" (
+    echo 文件 "!video2!" 不存在，请检查路径后重试。
+    pause
+    exit /b
+)
+
+echo.
+echo 正在执行视频合成命令，将第二个视频的视频流和音频流替换到第一个视频中...
+
+echo ffmpeg -i !video1! -i !video2! -map 1:v:0 -map 1:a -map 0:2 -map_metadata 0 -c copy -disposition:v:1 attached_pic output.mp4
+ffmpeg -i !video1! -i !video2! -map 1:v:0 -map 1:a -map 0:2 -map_metadata 0 -c copy -disposition:v:1 attached_pic output.mp4
+
+echo.
+echo 合成完成，生成文件为 output.mp4
+
+
+
+pause
+exit
