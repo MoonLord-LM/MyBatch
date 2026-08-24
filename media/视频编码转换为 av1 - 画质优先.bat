ec557501071f7fa2c51f98df56e49617774b5ed4
MoonLord-LM <me@moonlord.cn>
2026-08-24 18:31:44 +0800

    Update

--- media/视频编码转换为 av1 - 画质优先.bat
+++ media/视频编码转换为 av1 - 画质优先.bat
@@ -8,7 +8,7 @@ powershell -NoProfile -Command "Write-Host '[ !script_name_ext! ]' -ForegroundCo
 
 
 
-powershell -NoProfile -Command "Write-Host '视频编码转换为 av1 格式（高质量 -crf 16 -cpu-used 2）' -ForegroundColor Green"
+powershell -NoProfile -Command "Write-Host '视频编码转换为 av1 格式（高质量 -crf 16 -cpu-used 1）' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '使用 libaom-av1 编码器，色彩范围完整，但是速度较慢' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '双击运行时，自动递归扫描和处理当前文件夹下所有的视频文件' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '拖拽单个视频文件到此脚本上时，则只处理该文件；拖拽文件夹时，则递归处理其中所有文件' -ForegroundColor Green"
