c5305b03b95dc577d455e3ee580c877ddb197cf7
MoonLord-LM <me@moonlord.cn>
2026-08-25 12:41:39 +0800

    Update

--- file/文件名删除重复前后缀.bat
+++ file/文件名删除重复前后缀.bat
@@ -62,7 +62,9 @@ if not "!working_dir!" == "" (
     set /a "rename_failed=0"
 
     powershell -NoProfile -Command ^
-     "[Console]::OutputEncoding=[Text.Encoding]::UTF8; $files = @(Get-ChildItem -LiteralPath $env:file_path -File -Recurse | Where-Object { $_.Extension -match '^\.(jpg|jpeg|png|webp|bmp|gif|tif|tiff|heic|heif|avif|mp4|mkv|ts|avi|wmv|flv|rmvb|rm|vob|mpg|mpeg|3gp|m4v|f4v|mov|webm|ico)$' });" ^
+     "[Console]::OutputEncoding=[Text.Encoding]::UTF8;" ^
+     "$ext_filter = '^\.(jpg|jpeg|png|webp|bmp|gif|tif|tiff|heic|heif|avif|mp4|mkv|ts|avi|wmv|flv|rmvb|rm|vob|mpg|mpeg|3gp|m4v|f4v|mov|webm|ico)$';" ^
+     "$files = @(Get-ChildItem -LiteralPath $env:file_path -File -Recurse | Where-Object { $_.Extension -match $ext_filter });" ^
      "if ($files.Count -eq 0) { Write-Host '没有找到指定格式的文件'; exit 0 };" ^
      "Write-Host ('一共 ' + $files.Count + ' 个文件');" ^
      "foreach ($g in ($files | Group-Object Extension)) {" ^
