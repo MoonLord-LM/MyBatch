3c40fd3419efd2e69248919c818254cb40f92314
MoonLord-LM <me@moonlord.cn>
2026-08-25 15:53:37 +0800

    Update

--- system/垃圾文件清理.bat
+++ system/垃圾文件清理.bat
@@ -20,8 +20,9 @@ echo.
 
 
 
-set "log_file=!script_name!.log"
 echo 开始扫描
+echo.
+set "log_file=!script_name!.log"
 powershell -NoProfile -Command "[Console]::OutputEncoding=[Text.Encoding]::UTF8; ('-- ' + (Get-Date -Format 'yyyy-MM-dd HH:mm:ss') + ' --') | Out-File -LiteralPath $env:log_file -Append -Encoding UTF8"
 
 REM 龙之谷 DragonNest
@@ -59,7 +60,7 @@ for /f "tokens=2,*" %%a in ('reg query "HKEY_CURRENT_USER\Software\miHoYo\HYP\1_
         echo 正在清理文件夹："!root_dir!"
 
         set "temp_list=%temp%\MyBatch_%random%_%random%_%random%_%random%.tmp" & type nul > "!temp_list!"
-        dir /s /b /a-d "!root_dir!Genshin Impact Game\*.dmp" 2>nul >> "!temp_list!"
+        dir /s /b /a-d "!root_dir!*.dmp" 2>nul >> "!temp_list!"
         powershell -NoProfile -Command "[Console]::OutputEncoding=[Text.Encoding]::UTF8; Add-Type -AssemblyName Microsoft.VisualBasic; $files = Get-Content -Encoding UTF8 -LiteralPath $env:temp_list; $files | ForEach-Object { [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($_,'OnlyErrorDialogs','SendToRecycleBin') }; Write-Host ('删除完成，共删除 ' + $files.Count + ' 个文件') -ForegroundColor Green"
 
         type "!temp_list!" >> "!log_file!"
@@ -69,6 +70,7 @@ for /f "tokens=2,*" %%a in ('reg query "HKEY_CURRENT_USER\Software\miHoYo\HYP\1_
     endlocal
 )
 
+echo.
 echo 完成扫描，清理记录已保存到 "!log_file!" 文件中
 echo.>> "!log_file!"
 
