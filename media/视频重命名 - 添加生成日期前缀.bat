b6fdcf0bdcd70693a8dad297267221368cb5396c
MoonLord-LM <me@moonlord.cn>
2026-08-30 15:46:36 +0800

    Update

--- media/视频重命名 - 添加生成日期前缀.bat
+++ media/视频重命名 - 添加生成日期前缀.bat
@@ -8,7 +8,7 @@ powershell -NoProfile -Command "Write-Host '[ !script_name_ext! ]' -ForegroundCo
 
 
 
-powershell -NoProfile -Command "Write-Host '给视频文件添加生成日期前缀，默认使用系统时区' -ForegroundColor Green"
+powershell -NoProfile -Command "Write-Host '给视频文件添加生成日期前缀，优先使用内置的生成时间，默认使用系统时区' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '双击运行时，自动递归扫描和处理当前文件夹下所有的视频文件' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '拖拽单个视频文件到此脚本上时，则只处理该文件；拖拽文件夹时，则递归处理其中所有文件' -ForegroundColor Green"
 powershell -NoProfile -Command "Write-Host '支持的格式为 mp4 mkv ts avi wmv flv rmvb rm vob mpg mpeg 3gp m4v f4v mov webm' -ForegroundColor Green"
